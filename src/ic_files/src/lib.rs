dbd20d9bdba0b390a77d0e5394147237359c2999
Success Aje <successaje7@gmail.com>
2023-07-17 13:19:57 +0100

    rust canister init

---
+++ src/ic_files/src/lib.rs
@@ -0,0 +1,20 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
