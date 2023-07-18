df69e2bee25cc5241057a412a4bbfa820b3725c3
Success Aje <successaje7@gmail.com>
2023-07-18 21:06:01 +0100

    update

--- src/ic_files/src/lib.rs
+++ src/ic_files/src/lib.rs
@@ -2,12 +2,12 @@ use ic_cdk_macros::*;
 use ic_cdk::export::candid;
 #[import(canister = "ic_files_backend")]
 
-struct CounterCanister;
+// struct CounterCanister;
 
-[#update]
-async fn read() -> candid::Nat {
-    CounterCanister::read().await.0
-}
+// [#update]
+// async fn read() -> candid::Nat {
+//     CounterCanister::read().await.0
+// }
 
 #[ic_cdk::query]
 fn greet(name: String) -> String {
