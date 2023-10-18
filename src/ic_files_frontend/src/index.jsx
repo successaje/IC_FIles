9fc1bd5ef2bf10c27954fbaf11762d90eb797892
Success Aje <successaje7@gmail.com>
2023-10-18 22:22:17 +0100

    frontend feat

--- src/ic_files_frontend/src/index.jsx
+++ src/ic_files_frontend/src/index.jsx
@@ -28,43 +28,46 @@ const MyHello = () => {
   }
 
 return (
-
-
-  <div style={{ "fontSize": "30px" }}>
   <Header />
-  {/* Header */}
-  {/*auth == true
-      sidebar
-      filesView
-      sideicons
-   */}
-
-    
-      {/* <SideBar /> */}
-      <p>Greetings, from ICFiles!</p>
-      <p>
-        {" "}
-        Type your message in the Name input field, then click{" "}
-        <b> Get Greeting</b> to display the result.
-      </p>
-    <div style={{ margin: "30px" }}>
-      <input
-        id="name"
-        value={name}
-        onChange={(ev) => setName(ev.target.value)}
-      ></input>
-      {/* <input
-        id= */}
-      <button onClick={doGreet}>Get Greeting!</button>
-    </div>
-
-    <div>
-      Greeting is: "
-      <span style={{ color: "blue" }}>{message}</span>"
-    </div>
-  </div>
+  // <div className="App">
+  //   <SideBar />
+  // </div>
 );
 
+
+// <div style={{ "fontSize": "30px" }}>
+//   <Header />
+//   {/* Header */}
+//   {/*auth == true
+//       sidebar
+//       filesView
+//       sideicons
+//    */}
+
+    
+//       {/* <SideBar /> */}
+//       <p>Greetings, from ICFiles!</p>
+//       <p>
+//         {" "}
+//         Type your message in the Name input field, then click{" "}
+//         <b> Get Greeting</b> to display the result.
+//       </p>
+//     <div style={{ margin: "30px" }}>
+//       <input
+//         id="name"
+//         value={name}
+//         onChange={(ev) => setName(ev.target.value)}
+//       ></input>
+//       {/* <input
+//         id= */}
+//       <button onClick={doGreet}>Get Greeting!</button>
+//     </div>
+
+//     <div>
+//       Greeting is: "
+//       <span style={{ color: "blue" }}>{message}</span>"
+//     </div>
+//   </div>
 
 
 
